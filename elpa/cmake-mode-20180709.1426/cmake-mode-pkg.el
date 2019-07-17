8a55931ab6a2714a14e665fdb90fad9d28acf69b
glendai(戴敏) <glendai@tencent.com>
2019-07-17 12:48:23 +0800

    update elpa

--- elpa/cmake-mode-20180709.1426/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20180709.1426" "major-mode for editing CMake sources" 'nil :commit "5e2d22c177add56b2538ab10954dfb049c5cf945")
---
+++ elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "c3d9d800157a8fe2c0478d7e054544b99694c240")
